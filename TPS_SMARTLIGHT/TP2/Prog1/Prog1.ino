a882ecef07ac26fffb0f840d89bd43a586f04210
MasterFHGit <franck.buloup@univ-amu.fr>
2026-02-23 10:56:07 +0100

    TP2 continue

--- TPS_SMARTLIGHT/TP2/Prog1/Prog1.ino
+++ TPS_SMARTLIGHT/TP2/Prog1/Prog1.ino
@@ -1,4 +1,6 @@
-#define SAMPLE_PERIOD 500 // ms
+// TP2 - Prog 1, 1b et 1c
+
+#define SAMPLE_PERIOD 500 // en ms
 #define ANALOG_PIN A9
 
 bool firstLoop;
@@ -8,32 +10,30 @@ float sensorValueVolt;
 unsigned long time, t0, previousTime;
 
 void setup() {
-  //analogReference(AR_DEFAULT);
+  //analogReference(AR_INTERNAL1V0);
   analogReference(AR_DEFAULT);
   analogReadResolution(10);
   Serial.begin(9600);
   previousTime = -SAMPLE_PERIOD;
+  t0 = millis();
+  time = 0;
 }
 
 void loop() {
-  if(firstLoop) {
-    t0 = millis();
-    time = 0;
-    firstLoop = false;
-  } else time = millis() - t0;
+  time = millis() - t0;
 
   if(time - previousTime >= SAMPLE_PERIOD) {
     sensorValueInt = analogRead(ANALOG_PIN);
     Serial.print(sensorValueInt);
     Serial.print(" - ");
-    sensorValueVolt = sensorValueInt*3.3 / 1019;//map(sensorValueInt, 0, 4095, 0, 3300000);
+    sensorValueVolt = sensorValueInt*3.28 / 1017;
     Serial.println(sensorValueVolt);
     previousTime = time;
   }
   // AR_DEFAULT
   // Valeur min 0 pour 0V
-  // Valeur max 1019 pour 3.3V
-  // Valeur centrale : 510 pour 1.64V
-  // q = 3.3/1019 # 3.24mV
+  // Valeur max 1017 pour 3.28V
+  // Valeur centrale : 509 pour 1.64V
+  // q = 3.28/1017 # 3.22mV
   
 }
---
+++ TPS_SMARTLIGHT/TP2/Prog2/Prog2/Prog2.ino
@@ -0,0 +1,57 @@
+// TP2 - Prog 2
+
+#define SAMPLE_PERIOD 500 // en ms
+#define ANALOG_PIN A0
+
+float sensorValueLux;
+
+unsigned long time, t0, previousTime;
+
+void setup() {
+  //analogReference(AR_INTERNAL1V0);
+  analogReference(AR_DEFAULT);
+  analogReadResolution(10);
+  Serial.begin(9600);
+  previousTime = -SAMPLE_PERIOD;
+  t0 = millis();
+  time = 0;
+}
+
+void loop() {
+  time = millis() - t0;
+
+  if(time - previousTime >= SAMPLE_PERIOD) {
+    sensorValueLux = getPhotoTransistorLux();
+    Serial.println(sensorValueLux);
+    previousTime = time;
+  }
+}
+
+float getPhotoTransistorLux() {
+  // AR_DEFAULT
+  // Valeur min 0 pour 0V
+  // Valeur max 1023 pour (3.3 - q)V
+  // Valeur centrale : 512 pour 1.65V
+  // q = 3.3/1024 # 3.223mV
+  // Vlight1 = R1*IPCE => IPCE = Vlight1/R1 
+  // IPCE = Vlight1/51*10^(-3) A
+  // IPCE = 1000/51*Vlight1 en uA (1)
+  // Pour 10 lux IPCE/IPCEmax = 0.01
+  // Pour 100 lux IPCE/IPCEmax = 0.1
+  // ...
+  // Pour 10000 lux IPCE/IPCEmax = 10
+  // ...
+  // Ev(lux) = 1000*IPCE/IPCEmax
+  // Avec (1) :
+  // Ev(lux) = 1000000/(51*IPCEmax)*Vlight1
+  // IPCEmax en uA. On prendra 24uA
+  // Ev(lux) = 1000000/(51*24)*3.3/1024*N
+  // N étant le nombre donné par le CAN
+  // Ev (lux) = 3300000/1253376*N
+  // Ev (lux) # 2.6328891*N
+  static unsigned int valueInt;
+  static float value;
+  valueInt = analogRead(ANALOG_PIN);
+  value = 2.6328891*valueInt;
+  return value;
+}
