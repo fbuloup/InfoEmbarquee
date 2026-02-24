79191b44b65931d49577aa6e3fa1469caad15518
frank buloup <frank.buloup@univ-amu.fr>
2026-02-24 14:45:37 +0100

    Prog3, Prog4

---
+++ TPS_SMARTLIGHT/TP2/Prog4/Prog4.ino
@@ -0,0 +1,71 @@
+// TP2 - Prog 4
+
+#define SAMPLE_PERIOD 500 // en ms
+#define ANALOG_PIN A9
+#define BP1_PIN D2
+
+#define STATE_0 0
+#define STATE_1 1
+#define STATE_2 2
+
+const byte nbValuesToAcquire = 10;
+
+float analogValue;
+
+unsigned long time, t0, previousTime, nbValues;
+
+byte bp1, previousBp1, state, nbAcquiredValues;
+
+void setup() {
+  //analogReference(AR_INTERNAL1V0);
+  analogReference(AR_DEFAULT);
+  analogReadResolution(10);
+  pinMode(BP1_PIN, INPUT);
+
+  Serial.begin(115200);
+  while(!Serial) delay(10);
+
+  bp1 = 1;
+  previousBp1 = 1;
+
+  nbAcquiredValues = 0;
+  analogValue = 0;
+
+  previousTime = -SAMPLE_PERIOD;
+  t0 = millis();
+  time = 0;
+}
+
+void loop() {
+  time = millis() - t0;
+
+  if(time - previousTime >= SAMPLE_PERIOD) {
+
+    bp1 = digitalRead(BP1_PIN);    
+
+    if(bp1 == 0 && previousBp1 == 1 && state == STATE_0) state = STATE_1;
+    else if(nbAcquiredValues == nbValuesToAcquire && state == STATE_1) state = STATE_2;
+    else if(state == STATE_2) state = STATE_0;
+
+    switch (state) {      
+      case STATE_1:
+        analogValue += analogRead(ANALOG_PIN)*3.3/1024;
+        nbAcquiredValues++;
+        Serial.print("Acquisition number : ");
+        Serial.print(nbAcquiredValues);
+        Serial.print(" - CumSum Value : ");
+        Serial.println(analogValue);
+        break;
+      case STATE_2:
+        analogValue = analogValue/nbValuesToAcquire;
+        Serial.print("Mean value : ");
+        Serial.println(analogValue);
+        nbAcquiredValues = 0;
+        analogValue = 0;
+        break;
+    }
+
+    previousBp1 = bp1;    
+    previousTime = time;
+  }  
+}
