80f6f59e66a70b94a60ec389aeabfe9b99f0726b
frank buloup <frank.buloup@univ-amu.fr>
2026-03-30 21:57:01 +0200

    Refactor time variables for clarity in Prog2b.ino

--- TPS_SMARTLIGHT/TP2/Prog2b/Prog2b.ino
+++ TPS_SMARTLIGHT/TP2/Prog2b/Prog2b.ino
@@ -6,7 +6,8 @@ const unsigned int N = 10; // Taille de la moyenne glissante
 
 float sensorValueLux;
 
-unsigned long time, t0, previousTime;
+unsigned long time;
+long previousTime;
 
 void setup() {
   //analogReference(AR_INTERNAL1V0);
@@ -14,12 +15,10 @@ void setup() {
   analogReadResolution(10);
   Serial.begin(9600);
   previousTime = -SAMPLE_PERIOD;
-  t0 = millis();
-  time = 0;
 }
 
 void loop() {
-  time = millis() - t0;
+  time = millis();
 
   if(time - previousTime >= SAMPLE_PERIOD) {
     sensorValueLux = getPhotoTransistorLux();
